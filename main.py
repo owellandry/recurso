97abb4def79f1836d8e944521a20d78f6a605b96
owell andry <81540116+owellandry@users.noreply.github.com>
2023-04-15 15:44:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import time
-import random
-import requests
-import json
+#python3 -m pip install pyautogui
+#sudo apt-get install scrot
+#sudo apt-get install python3-tk
+#sudo apt-get install python3-dev
 
-#captcha
-#_grecaptcha:"09AMqPRJxWlovGLU4ds3xKgQQtInnc6ZZVH8raJ3f8nLGycBp-OFuPexjDmzkze272Ih2ygMSoe37df7ahYnrL-AUwPNnPxS42us0"
+import pyautogui as pg
 
-
-datos = { 'idcan':42, 'idcat':5 }
-headers = {'Content-Type': 'application/json'}
-cont=1
-
+count = 0
 while True:
-    time.sleep(random.uniform(107, 208))  
-    peticion = requests.post("https://www.vanguardia.com/empresasgeneradoras/reconocimientos/votar", headers=headers, data=json.dumps(datos))
-    cont=cont+1
-    print(f"voto, {cont}")
-    print(peticion.text)
-    if(cont == 120):
+    count=count+1
+    pg.sleep(3)
+    pg.click()
+    pg.rightClick()
+    pg.press("down")
+    pg.press("enter")
+    print(count)
+    if count==80:
         break
+
+#let div = document.querySelector('[data-id="42"]');
+#div.click();
