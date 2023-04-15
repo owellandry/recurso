5d17526f796d71db168d8a70a72543264cb57d15
owell andry <81540116+owellandry@users.noreply.github.com>
2023-04-15 11:31:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import pyautogui
 import time
 import random
 
+# Mueve el cursor a la posición deseada y lo mantiene allí
 x, y = 1780, 160
 pyautogui.moveTo(x, y)
-pyautogui.FAILSAFE = False
-
-clics_realizados = 0
-
+pyautogui.FAILSAFE = False  # Desactiva la función de seguridad
+cont=1
 while True:
+    # Hace clic en el botón tres veces con un intervalo de 3 a 5 segundos entre cada clic
     for i in range(3):
         pyautogui.click(button='left')
-        clics_realizados += 1
-        time.sleep(random.uniform(10, 20)) 
-        print("Se ha realizado el clic número", clics_realizados)
-        if(const == 200):
-            breack
+        time.sleep(random.uniform(107, 208))  
+        print(f"voto, {cont}")
+        if(cont == 200):
+            break
